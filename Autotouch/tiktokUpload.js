0dca1c99cc7125cc2cd942817a96f5cf4d57a24e
Valiant <rainguy.shy@gmail.com>
2023-11-18 05:42:27 -0700

    select the latest media

--- Autotouch/tiktokUpload.js
+++ Autotouch/tiktokUpload.js
@@ -1,5 +1,5 @@
 
-const { usleep, appRun, appKill, appState, appInfo, touchDown, touchMove, touchUp, toast } = at
+const { usleep, appRun, appKill, appState, appInfo, touchDown, touchMove, touchUp, toast, getColor } = at
 
 
 function swipeVertically(times) {
@@ -13,6 +13,26 @@ function swipeVertically(times) {
         usleep(500000);
     }
 }
+/**
+* Select the latest Media
+* @param {Number} num1
+* @param {Number} um
+* @return {Number} sum
+*/
+function latestMedia(x, y) {
+    let [color, error] = getColor(x, y)
+    if (error == null) {
+        if (color != "16777215") {
+            tap(1, x, y)
+        } else {
+            // alert(color);
+            latestMedia(x - 100, y)
+
+        }
+
+    }
+
+}
 
 function tap(no, x, y) {
     for (i = 0; i < no; i++) {
@@ -22,11 +42,16 @@ function tap(no, x, y) {
     }
 }
 function tikTokUpload() {
+    //tap bottom upload button 
     tap(1, 325, 1300)
     usleep(2000000)
+    // tap media button
     tap(1, 625, 1100)
     usleep(2000000)
-    tap(1, 625, 1100)
+    //select the latest media
+    latestMedia(625, 1100);
+
+
 }
 let tiktok = "com.zhiliaoapp.musically";
 appKill(tiktok);
@@ -37,6 +62,7 @@ swipeVertically(1);
 usleep(500000);
 toast("uploading");
 tikTokUpload();
+
 
 const tikTokState = appState(tiktok);
 
