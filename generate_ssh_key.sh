0bc097277a4482082e1ab503fd123bf430acf0ad
tinysoda <rainguy.shy@gmail.com>
2023-11-26 12:18:08 +0700

    added github ssh script

---
+++ generate_ssh_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to generate SSH key and display the public key
+
+# Set the email address
+email="rainguy.shy@gmail.com"
+
+# Generate SSH key
+ssh-keygen -o -t rsa -C "$email"
+
+# Display the public key
+cat ~/.ssh/id_rsa.pub
