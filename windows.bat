3737ab22d0f01302ca3cb416e98e8d8a01aab1be
Valiant <rainguy.shy@gmail.com>
2024-02-27 16:56:17 +0700

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -20,4 +20,8 @@ echo Installing Thorium Browser...
 curl -o thorium.exe -L https://github.com/Alex313031/Thorium-Win/releases/download/M121.0.6167.204/thorium_AVX2_mini_installer.exe
 start /wait thorium.exe /S
 
+echo Installing Python 3...
+curl -o python3.exe -L https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe
+start /wait python3.exe /quiet InstallAllUsers=1 PrependPath=1
+
 echo Installation completed.
