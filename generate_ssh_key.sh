d4ed7a38c7c9435394a6257a6913368fdb9a26f4
tinysoda <rainguy.shy@gmail.com>
2024-05-22 11:09:22 +0700

    update ssh script

--- generate_ssh_key.sh
+++ generate_ssh_key.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-# Script to generate SSH key and display the public key
+# Script to configure Git, generate SSH key, display the public key, and open GitHub SSH keys settings page
 
-# Set the email address
+# Set the email address and username
 email="rainguy.shy@gmail.com"
 user="tinysoda"
 
-# Generate SSH key
-ssh-keygen -o -t rsa -C "$email"
+# Configure Git
+echo "Configuring Git with username and email..."
+git config --global user.name "$user"
+git config --global user.email "$email"
+echo "Git configured."
+
+# Define the default file for the SSH key
+KEY_FILE="$HOME/.ssh/id_rsa"
+
+# Check if the SSH key already exists
+if [ -f "$KEY_FILE" ]; then
+  echo "An SSH key already exists at $KEY_FILE."
+  echo "Here is the public key:"
+else
+  # Generate a new SSH key
+  ssh-keygen -o -t rsa -b 4096 -C "$email" -f "$KEY_FILE" -N ""
+  echo "SSH key generated."
+fi
 
 # Display the public key
-cat ~/.ssh/id_rsa.pub
+cat "${KEY_FILE}.pub"
 
 # Open the browser and go to GitHub SSH keys settings page
 if command -v xdg-open &> /dev/null; then
