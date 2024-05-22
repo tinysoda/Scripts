17bb1c61ad77e9e98060a5dc141377c5c4cbff27
Truong Giang <rainguy.shy@gmail.com>
2024-05-22 11:06:09 +0700

    update ssh script

--- generate_ssh_key.sh
+++ generate_ssh_key.sh
@@ -11,3 +11,15 @@ ssh-keygen -o -t rsa -C "$email"
 
 # Display the public key
 cat ~/.ssh/id_rsa.pub
+
+# Open the browser and go to GitHub SSH keys settings page
+if command -v xdg-open &> /dev/null; then
+  xdg-open https://github.com/settings/keys
+elif command -v gnome-open &> /dev/null; then
+  gnome-open https://github.com/settings/keys
+elif command -v open &> /dev/null; then
+  open https://github.com/settings/keys
+else
+  echo "Please open the following URL in your browser to add your new SSH key:"
+  echo "https://github.com/settings/keys"
+fi
