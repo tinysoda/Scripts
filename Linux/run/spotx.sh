5207ff92d2e639fa6a8c5159fb5601bbba72611b
tinysoda <rainguy.shy@gmail.com>
2026-08-28 07:45:31 +0700

    spotify check

--- Linux/run/spotx.sh
+++ Linux/run/spotx.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-paru -Sy spotify zip --noconfirm
-curl -sSL https://spotx-official.github.io/run.sh | bash
+if command -v spotify >/dev/null 2>&1 && command -v zip >/dev/null 2>&1; then
+    echo "Spotify and zip are already installed. Skipping installation."
+else
+    echo "Spotify or zip is missing. Installing..."
+    paru -Sy spotify zip --noconfirm
+fi
 
+curl -sSL https://spotx-official.github.io/run.sh | bash
